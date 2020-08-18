fc5434c8d3d6227108bed3743e55c9aa2fd8f332
Matt Hill <matt.hill.email@gmail.com>
2020-08-18 00:37:27 -0700

    asdf

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -18,11 +18,10 @@ export default function HomePage({ history }) {
             src="/assets/logo.png"
             style={{ marginBottom: 12 }}
           />
-          You're seeing this because you've successfully setup Firebase Hosting.
-          Now it's time to go build something extraordinary!
+          Troll Need Gold
         </Header>
         <Button onClick={() => history.push("/events")} size="huge" inverted>
-          Get started
+          Troll Want Gold...?
           <Icon name="right arrow" inverted />
         </Button>
       </Container>
