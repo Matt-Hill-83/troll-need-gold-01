1045e3b10ec03356e1d52ce7771c6ff0b403f050
Matt Hill <matt.hill.email@gmail.com>
2020-08-18 01:03:43 -0700

    move to troll-need-gold-02

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -5,22 +5,23 @@ import "firebase/auth"
 import "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDaAb3NresAA5R6ZwpOc9UR7B00MPQanXU",
-  authDomain: "revents001.firebaseapp.com",
-  databaseURL: "https://revents001.firebaseio.com",
-  projectId: "revents001",
-  storageBucket: "revents001.appspot.com",
-  messagingSenderId: "527222668637",
-  appId: "1:527222668637:web:c31d492c0780617c2d3cb4",
+  apiKey: "AIzaSyBvEIUMp8ozrTc_kZ6-5i5Q7aOxXzF9hXU",
+  authDomain: "troll-need-gold-02.firebaseapp.com",
+  databaseURL: "https://troll-need-gold-02.firebaseio.com",
+  projectId: "troll-need-gold-02",
+  storageBucket: "troll-need-gold-02.appspot.com",
+  messagingSenderId: "1034476036518",
+  appId: "1:1034476036518:web:acf0c6908f6788f2d0af67",
+  measurementId: "G-KDMHMY1XL4",
 }
 // const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_API_KEY,
-//     authDomain: "reventscourse.firebaseapp.com",
-//     databaseURL: "https://reventscourse.firebaseio.com",
-//     projectId: "reventscourse",
-//     storageBucket: "reventscourse.appspot.com",
-//     messagingSenderId: "990229111437",
-//     appId: "1:990229111437:web:cb40dfdf45505da5b7f8c4"
+//   apiKey: "AIzaSyDaAb3NresAA5R6ZwpOc9UR7B00MPQanXU",
+//   authDomain: "revents001.firebaseapp.com",
+//   databaseURL: "https://revents001.firebaseio.com",
+//   projectId: "revents001",
+//   storageBucket: "revents001.appspot.com",
+//   messagingSenderId: "527222668637",
+//   appId: "1:527222668637:web:c31d492c0780617c2d3cb4",
 // }
 
 firebase.initializeApp(firebaseConfig)
--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,53 +1,55 @@
-import React from 'react';
-import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
-import NavBar from '../../features/nav/NavBar';
-import { Container } from 'semantic-ui-react';
-import { Route, useLocation } from 'react-router-dom';
-import HomePage from '../../features/home/HomePage';
-import EventDetailedPage from '../../features/events/eventDetailed/EventDetailedPage';
-import EventForm from '../../features/events/eventForm/EventForm';
-import Sandbox from '../../features/sandbox/Sandbox';
-import ModalManager from '../common/modals/ModalManager';
-import { ToastContainer } from 'react-toastify';
-import ErrorComponent from '../common/errors/ErrorComponent';
-import AccountPage from '../../features/auth/AccountPage';
-import { useSelector } from 'react-redux';
-import LoadingComponent from './LoadingComponent';
-import ProfilePage from '../../features/profiles/profilePage/ProfilePage';
-import PrivateRoute from './PrivateRoute';
+import { Route, useLocation } from "react-router-dom"
+
+import AccountPage from "../../features/auth/AccountPage"
+import { Container } from "semantic-ui-react"
+import ErrorComponent from "../common/errors/ErrorComponent"
+import EventDashboard from "../../features/events/eventDashboard/EventDashboard"
+import EventDetailedPage from "../../features/events/eventDetailed/EventDetailedPage"
+import EventForm from "../../features/events/eventForm/EventForm"
+import HomePage from "../../features/home/HomePage"
+import LoadingComponent from "./LoadingComponent"
+import ModalManager from "../common/modals/ModalManager"
+
+import NavBar from "../../features/nav/NavBar"
+import PrivateRoute from "./PrivateRoute"
+import ProfilePage from "../../features/profiles/profilePage/ProfilePage"
+import React from "react"
+import Sandbox from "../../features/sandbox/Sandbox"
+import { ToastContainer } from "react-toastify"
+import { useSelector } from "react-redux"
 
 export default function App() {
-  const { key } = useLocation();
-  const { initialized } = useSelector((state) => state.async);
+  const { key } = useLocation()
+  const { initialized } = useSelector((state) => state.async)
 
-  if (!initialized) return <LoadingComponent content='Loading app...' />
+  if (!initialized) return <LoadingComponent content="Loading app..." />
 
   return (
     <>
       <ModalManager />
-      <ToastContainer position='bottom-right' hideProgressBar />
-      <Route exact path='/' component={HomePage} />
+      <ToastContainer position="bottom-right" hideProgressBar />
+      <Route exact path="/" component={HomePage} />
       <Route
-        path={'/(.+)'}
+        path={"/(.+)"}
         render={() => (
           <>
             <NavBar />
-            <Container className='main'>
-              <Route exact path='/events' component={EventDashboard} />
-              <Route exact path='/sandbox' component={Sandbox} />
-              <Route path='/events/:id' component={EventDetailedPage} />
+            <Container className="main">
+              <Route exact path="/events" component={EventDashboard} />
+              <Route exact path="/sandbox" component={Sandbox} />
+              <Route path="/events/:id" component={EventDetailedPage} />
               <PrivateRoute
-                path={['/createEvent', '/manage/:id']}
+                path={["/createEvent", "/manage/:id"]}
                 component={EventForm}
                 key={key}
               />
-              <PrivateRoute path='/account' component={AccountPage} />
-              <PrivateRoute path='/profile/:id' component={ProfilePage} />
-              <Route path='/error' component={ErrorComponent} />
+              <PrivateRoute path="/account" component={AccountPage} />
+              <PrivateRoute path="/profile/:id" component={ProfilePage} />
+              <Route path="/error" component={ErrorComponent} />
             </Container>
           </>
         )}
       />
     </>
-  );
+  )
 }
