bfcc67bcb2e3450a0b2e2974c89776b512a0a0d0
Matt Hill <matt.hill.email@gmail.com>
2020-08-18 00:26:05 -0700

    hosting works

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
-import { Segment, Container, Header, Image, Button, Icon } from 'semantic-ui-react';
+import React from "react"
+import {
+  Segment,
+  Container,
+  Header,
+  Image,
+  Button,
+  Icon,
+} from "semantic-ui-react"
 
-export default function HomePage({history}) {
-    return (
-        <Segment inverted textAlign='center' vertical className='masthead'>
-            <Container>
-                <Header as='h1' inverted>
-                    <Image size='massive' src='/assets/logo.png' style={{marginBottom: 12}} />
-                    Re-vents
-                </Header>
-                <Button onClick={() => history.push('/events')} size='huge' inverted>
-                    Get started
-                    <Icon name='right arrow' inverted />
-                </Button>
-            </Container>
-        </Segment>
-    )
+export default function HomePage({ history }) {
+  return (
+    <Segment inverted textAlign="center" vertical className="masthead">
+      <Container>
+        <Header as="h1" inverted>
+          <Image
+            size="massive"
+            src="/assets/logo.png"
+            style={{ marginBottom: 12 }}
+          />
+          You're seeing this because you've successfully setup Firebase Hosting.
+          Now it's time to go build something extraordinary!
+        </Header>
+        <Button onClick={() => history.push("/events")} size="huge" inverted>
+          Get started
+          <Icon name="right arrow" inverted />
+        </Button>
+      </Container>
+    </Segment>
+  )
 }
