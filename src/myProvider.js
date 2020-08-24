e065e948158dd8acbd41eb982ba294802747377e
Matt Hill <matt.hill.email@gmail.com>
2020-08-24 05:20:20 -0700

    asdf

--- src/myProvider.js
+++ src/myProvider.js
@@ -3,9 +3,6 @@ import React, { useState, createContext } from "react"
 export const myContext = createContext()
 
 export const MyProvider = (props) => {
-  console.log("props", props) // zzz
-
-  console.log("props.children", props.children) // zzz
   const [localStorage, setLocalStorage] = useState({
     number: 0,
 
@@ -15,24 +12,7 @@ export const MyProvider = (props) => {
     defaultWorldId: null,
     showBookPicker: false,
     showWorldBuilder: false,
-
-    // _defaultQuestStatus: {
-    //   visitedScenes: [],
-    //   completedMissions: [],
-    //   lockedScenes: [],
-    //   hiddenScenes: [],
-    //   cloudedScenes: [],
-    //   activeSubQuestIndex: 0,
-    //   activeMissionIndex: 0,
-    //   pockets: { gold: { amount: 0 } },
-    //   desiredItems: [],
-    //   questConfig: {
-    //     missions: [],
-    //   },
-    // },
   })
-
-  // const [localStorage, setLocalStorage] = useState({ number: 0, test: 555 })
 
   return (
     <myContext.Provider value={[localStorage, setLocalStorage]}>
