0254dc45743776dfec38f394f49e319638fca47a
Matt Hill <matt.hill.email@gmail.com>
2020-08-29 07:35:10 -0700

    extract useContext to function

--- src/oldProject/components/TopLevel/TopLevel.js
+++ src/oldProject/components/TopLevel/TopLevel.js
@@ -18,43 +18,18 @@ const toaster = Toaster.create({
 })
 
 export default function TopLevel(props) {
-  const { localStorage, setLocalStorage, setDesiredItems } = useLocalStorage()
+  const {
+    localStorage,
+    increaseNumber,
+    setLocalStorage,
+    setDesiredItems,
+    setLocalStorageProp2,
+  } = useLocalStorage()
 
   const [localProps, setLocalProps] = useState({})
   console.log("localProps", localProps) // zzz
 
   console.log("localStorage-----------------------------------1", localStorage)
-
-  const increaseNumber = () => {
-    setLocalStorage((prevVal) => {
-      const newVal = { ...prevVal }
-      newVal.number += 1
-      return newVal
-    })
-  }
-
-  // const setDesiredItems = (props) => {
-  //   setLocalStorage((prevVal) => {
-  //     const newVal = { ...prevVal, test: props }
-  //     // return { asdf: 324 }
-  //     return newVal
-  //   })
-  // }
-
-  const setLocalStorageProp2 = (props) => {
-    console.log("") // zzz
-    console.log("") // zzz
-    console.log("setLocalStorageProp2---------------------------->") // zzz
-    // console.log("props", props) // zzz
-    setLocalStorage((state) => {
-      // console.log("state", state) // zzz
-      const test = { ...state, ...props, test: new Date() }
-      // console.log("test.world", test.world) // zzz
-      return test
-    })
-    console.log("localStorage --- just set", localStorage) // zzz
-    // console.log("localStorage --- just set", localStorage.world) // zzz
-  }
 
   // on mount
   useEffect(() => {
