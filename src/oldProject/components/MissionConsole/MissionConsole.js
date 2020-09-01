b8a7211599f4c18db9ad80347ac3c7005d7dc3a7
Matt Hill <matt.hill.email@gmail.com>
2020-09-01 07:35:53 -0700

    asdf

--- src/oldProject/components/MissionConsole/MissionConsole.js
+++ src/oldProject/components/MissionConsole/MissionConsole.js
@@ -78,7 +78,6 @@ export default function MissionConsole(props) {
 
   return (
     <div className={css.main}>
-      {/* activeMissionIndex: {activeMissionIndex} */}
       <div className={css.body}>
         <div className={css.row}>
           <div className={css.left}>
