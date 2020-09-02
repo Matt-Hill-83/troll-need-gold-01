0d75d40debf873068a2d0297c7d8a153dfedde81
Matt Hill <matt.hill.email@gmail.com>
2020-09-02 06:10:56 -0700

    asdf

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -14,15 +14,6 @@ const firebaseConfig = {
   appId: "1:1034476036518:web:acf0c6908f6788f2d0af67",
   measurementId: "G-KDMHMY1XL4",
 }
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDaAb3NresAA5R6ZwpOc9UR7B00MPQanXU",
-//   authDomain: "revents001.firebaseapp.com",
-//   databaseURL: "https://revents001.firebaseio.com",
-//   projectId: "revents001",
-//   storageBucket: "revents001.appspot.com",
-//   messagingSenderId: "527222668637",
-//   appId: "1:527222668637:web:c31d492c0780617c2d3cb4",
-// }
 
 firebase.initializeApp(firebaseConfig)
 firebase.firestore()
--- src/features/profiles/profilePage/ProfileForm.jsx
+++ src/features/profiles/profilePage/ProfileForm.jsx
@@ -1,48 +1,55 @@
-import React from 'react';
-import { Formik, Form } from 'formik';
-import MyTextInput from '../../../app/common/form/MyTextInput';
-import MyTextArea from '../../../app/common/form/MyTextArea';
-import { Button } from 'semantic-ui-react';
-import * as Yup from 'yup';
-import { toast } from 'react-toastify';
-import { updateUserProfile } from '../../../app/firestore/firestoreService';
+import React from "react"
+import { Button } from "semantic-ui-react"
+import { Formik, Form } from "formik"
+import { toast } from "react-toastify"
+import * as Yup from "yup"
 
-export default function ProfileForm({profile}) {
-    return (
-        <Formik
-            initialValues={{
-                displayName: profile.displayName,
-                description: profile.description || ''
-            }}
-            validationSchema={Yup.object({
-                displayName: Yup.string().required()
-            })}
-            onSubmit={async (values, {setSubmitting}) => {
-                try {
-                    await updateUserProfile(values);
-                } catch (error) {
-                    toast.error(error.message);
-                } finally {
-                    setSubmitting(false);
-                }
-            }}
-        >
-            {({isSubmitting, isValid, dirty}) => (
-                <Form className='ui form'>
-                <MyTextInput name='displayName' placeholder='Display Name' />
-                <MyTextArea name='description' placeholder='Description' />
-                <Button 
-                    loading={isSubmitting}
-                    disabled={isSubmitting || !isValid || !dirty}
-                    floated='right'
-                    type='submit'
-                    size='large'
-                    positive
-                    content='Update profile'
-                />
-            </Form>
-            )}
-            
-        </Formik>
-    )
+import { updateUserProfile } from "../../../app/firestore/firestoreService"
+import MyTextArea from "../../../app/common/form/MyTextArea"
+import MyTextInput from "../../../app/common/form/MyTextInput"
+
+export default function ProfileForm({ profile }) {
+  console.log("profile", profile) // zzz
+  return (
+    <Formik
+      initialValues={{
+        displayName: profile.displayName,
+        description: profile.description || "",
+        cat: profile.cat || "",
+      }}
+      validationSchema={Yup.object({
+        displayName: Yup.string().required(),
+      })}
+      onSubmit={async (values, { setSubmitting }) => {
+        try {
+          await updateUserProfile(values)
+        } catch (error) {
+          toast.error(error.message)
+        } finally {
+          setSubmitting(false)
+        }
+      }}
+    >
+      {({ isSubmitting, isValid, dirty }) => (
+        <Form className="ui form">
+          <MyTextInput name="displayName" placeholder="Display Name" />
+          <MyTextArea
+            name="description"
+            placeholder="Description"
+            label="description"
+          />
+          <MyTextArea name="cat" placeholder="Cat" label="cat" />
+          <Button
+            loading={isSubmitting}
+            disabled={isSubmitting || !isValid || !dirty}
+            floated="right"
+            type="submit"
+            size="large"
+            positive
+            content="Update profile"
+          />
+        </Form>
+      )}
+    </Formik>
+  )
 }
