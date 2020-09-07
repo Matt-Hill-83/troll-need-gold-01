6586fe854c5d2be480afa346fd8fc23270e12119
Matt Hill <matt.hill.email@gmail.com>
2020-09-07 08:26:22 -0700

    asf

--- src/oldProject/components/BookTableOfContents/BookTableOfContents.js
+++ src/oldProject/components/BookTableOfContents/BookTableOfContents.js
@@ -8,57 +8,47 @@ import Utils from "../../Utils/Utils.js"
 
 import css from "./BookTableOfContents.module.scss"
 
-class BookTableOfContents extends React.Component {
-  state = {}
+export default function BookTableOfContents(props) {
+  const { worlds, selectedBook } = props
+  const sortedWorlds = Utils.sortWorlds({ worlds })
 
-  render = () => {
-    const { worlds, selectedBook } = this.props
-    const sortedWorlds = Utils.sortWorlds({ worlds })
+  const filteredWorlds = sortedWorlds.filter((world) => {
+    const chapters = _get(selectedBook, "chapters") || []
+    return world.released && chapters.includes(world.id)
+  })
 
-    const filteredWorlds = sortedWorlds.filter((world) => {
-      const chapters = _get(selectedBook, "chapters") || []
-      return world.released && chapters.includes(world.id)
-    })
+  const worldList = filteredWorlds.map((world) => {
+    const { title, id: worldId } = world
 
-    const worldList = filteredWorlds.map((world) => {
-      const { title, id: worldId } = world
-
-      const text = (
-        <div className={css.questRow}>
-          <div className={cx(css.tableCell, css.questName)}>{title}</div>
-          <div className={cx(css.tableCell, css.dragonPoints)}>100 </div>
-          <div className={cx(css.tableCell, css.questStatus)}>
-            <span role="img">✅</span>
-          </div>
+    const text = (
+      <div className={css.questRow}>
+        <div className={cx(css.tableCell, css.questName)}>{title}</div>
+        <div className={cx(css.tableCell, css.dragonPoints)}>100 </div>
+        <div className={cx(css.tableCell, css.questStatus)}>
+          <span role="img">✅</span>
         </div>
-      )
-
-      return <Link to={`/quests/${worldId}`}>{text}</Link>
-    })
-
-    const tableHeader = (
-      <div className={cx(css.tableHeader)}>
-        <div className={cx(css.tableCell, css.name)}>Name</div>
-        <div className={cx(css.tableCell, css.gold)}>Gold</div>
-        <div className={cx(css.tableCell, css.status)}>Completed</div>
       </div>
     )
-    const dummyQuest01 = Images.backgrounds["dummyQuest01"]
 
-    return (
-      <div className={css.main}>
-        <div className={css.questTable}>
-          {tableHeader}
-          <div className={css.scrollArea}>{worldList}</div>
-        </div>
-        <img
-          className={cx(css.dummyQuest01)}
-          src={dummyQuest01}
-          alt={"imagex"}
-        />
+    return <Link to={`/quests/${worldId}`}>{text}</Link>
+  })
+
+  const tableHeader = (
+    <div className={cx(css.tableHeader)}>
+      <div className={cx(css.tableCell, css.name)}>Name</div>
+      <div className={cx(css.tableCell, css.gold)}>Gold</div>
+      <div className={cx(css.tableCell, css.status)}>Completed</div>
+    </div>
+  )
+  const dummyQuest01 = Images.backgrounds["dummyQuest01"]
+
+  return (
+    <div className={css.main}>
+      <div className={css.questTable}>
+        {tableHeader}
+        <div className={css.scrollArea}>{worldList}</div>
       </div>
-    )
-  }
+      <img className={cx(css.dummyQuest01)} src={dummyQuest01} alt={"imagex"} />
+    </div>
+  )
 }
-
-export default BookTableOfContents
