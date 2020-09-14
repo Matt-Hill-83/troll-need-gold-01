7e7f3acd742a894a2f7882832e99acd4e9405afe
Matt Hill <matt.hill.email@gmail.com>
2020-09-14 00:05:55 -0700

    asfd

--- src/features/quests/eventDetailed/QuestDetailedPage.jsx
+++ src/features/quests/eventDetailed/QuestDetailedPage.jsx
@@ -1,17 +1,14 @@
-import { Redirect } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
 import React from "react"
 
 import { listenToQuestFromFirestore } from "../../../app/firestore/firestoreService"
 import { listenToSelectedQuest } from "../questActions"
-import LoadingComponent from "../../../app/layout/LoadingComponent"
 import useFirestoreDoc from "../../../app/hooks/useFirestoreDoc"
 import TopLevel from "../../../oldProject/components/TopLevel/TopLevel"
 
 export default function QuestDetailedPage({ match }) {
   const dispatch = useDispatch()
   const quest = useSelector((state) => state.quest.selectedEvent)
-  const { loading, error } = useSelector((state) => state.async)
 
   useFirestoreDoc({
     query: () => listenToQuestFromFirestore(match.params.id),
@@ -20,8 +17,6 @@ export default function QuestDetailedPage({ match }) {
     },
     deps: [match.params.id, dispatch],
   })
-
-  if (error) return <Redirect to="/error" />
 
   return <TopLevel quest={quest}></TopLevel>
 }
