1842abb0781148a7bafbd7f6db9f170ae467d960
Matt Hill <matt.hill.email@gmail.com>
2020-09-23 03:31:54 -0700

    asdf

--- src/oldProject/components/MyJsonEditor/MyJsonEditor.js
+++ src/oldProject/components/MyJsonEditor/MyJsonEditor.js
@@ -1,23 +1,14 @@
 import { ButtonGroup, Button } from "@blueprintjs/core"
-import { IconNames } from "@blueprintjs/icons"
-import cx from "classnames"
 import React, { useState, useEffect } from "react"
-
 import JSONEditorDemo from "../JsonEdtor/JSONEditorDemo.js"
-
-// import { updateQuestInFirestore } from "../../../app/firestore/firestoreService.js"
 
 import css from "./MyJsonEditor.module.scss"
 
-let world = {}
-
 export default function MyJsonEditor(props) {
-  world = props.world || {}
   const [jsonUnderEdit, setJsonUnderEdit] = useState(props.json || {})
 
   useEffect(() => {
     console.log("onMount-------------------------------->>>>")
-    world = props.world
 
     // returned function will be called on component unmount
     return () => {
@@ -41,13 +32,13 @@ export default function MyJsonEditor(props) {
 
   return (
     <div className={css.bookEditor} title={"Edit Book"}>
-      <ButtonGroup className={css.buttonGroup} color="primary">
-        <Button onClick={() => saveChanges({ jsonUnderEdit })}>
-          Save Changes
-        </Button>
-      </ButtonGroup>
       <div className="contents">
         <JSONEditorDemo json={jsonUnderEdit} onChangeJSON={onChangeJSON} />
+        <ButtonGroup className={css.buttonGroup} color="primary">
+          <Button onClick={() => saveChanges({ jsonUnderEdit })}>
+            Save Changes
+          </Button>
+        </ButtonGroup>
       </div>
     </div>
   )
