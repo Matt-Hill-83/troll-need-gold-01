c86896509ab23ea13f3d509aabb34157783afc72
Matt Hill <matt.hill.email@gmail.com>
2020-10-10 06:08:03 -0700

    asdf

--- src/Common/images/newBackgrounds/newBackgrounds.jsx
+++ src/Common/images/newBackgrounds/newBackgrounds.jsx
@@ -3,6 +3,9 @@ import back02 from "./back02.jpg"
 import back03 from "./back03.png"
 import back04 from "./back04.jpg"
 import back05 from "./back05.jpg"
+import back06 from "./back06.jpg"
+import back07 from "./back07.jpg"
+import back08 from "./back08.jpg"
 
 const newBackgrounds = {
   back02,
@@ -10,6 +13,9 @@ const newBackgrounds = {
   back04,
   back01,
   back05,
+  back06,
+  back07,
+  back08,
 }
 
 export { newBackgrounds }
