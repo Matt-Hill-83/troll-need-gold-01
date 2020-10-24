2c94d7b69c3e5530059a6d0a419d97c535d2f476
Matt Hill <matt.hill.email@gmail.com>
2020-10-24 05:51:46 -0700

    convert Character to function

--- src/Common/Components/Character/Character.js
+++ src/Common/Components/Character/Character.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React from "react"
 
 import Images from "../../../Common/Images/images"
 import Head from "../Head/Head"
@@ -8,72 +8,66 @@ import css from "./Character.module.scss"
 
 const girlImages = Images.posableCharacters
 
-// TODO - the else logic should be placed where this component is called?
+export default function Character(props) {
+  const {
+    headClassName,
+    showHeadOnly,
+    name,
+    mood,
+    isEditMode,
+    flipImage = false,
+  } = props
 
-class Character extends Component {
-  render() {
-    const {
-      headClassName,
-      showHeadOnly,
-      name,
-      mood,
-      isEditMode,
-      flipImage = false,
-    } = this.props
+  const images = girlImages.find((girl) => girl.name === name)
 
-    const images = girlImages.find((girl) => girl.name === name)
-
-    // if there are no posable images
-    if (!images) {
-      const image = Images.all[name]
-      if (!image) return null
-      const item = { name, id: name, flipImage }
-      return (
-        <div className={css.characterContainer}>
-          <ImageDisplay
-            item={item}
-            className={css.characterImage}
-            showLabel={true}
-          />
-        </div>
-      )
-    }
-
-    // else...
-    const {
-      images: { heads, body },
-    } = images
-
-    const head = heads.find((head) => head.mood === mood) || heads[0]
-
-    const className = showHeadOnly
-      ? css.headOnly
-      : `${css.headForBody} ${isEditMode ? "" : css.noBorder}`
-
-    const headOnly = (
-      <>
-        <Head
-          name={name}
-          head={head}
-          className={headClassName || className}
-          isEditMode={isEditMode}
-        />
-        <span className={`${css.bodyLabel}`}>{name}</span>
-      </>
-    )
-
-    if (showHeadOnly) {
-      return headOnly
-    }
-
+  // if there are no posable images
+  if (!images) {
+    const image = Images.all[name]
+    if (!image) return null
+    const item = { name, id: name, flipImage }
     return (
-      <div className={css.girlBodyContainer}>
-        <img className={`${css.girlBodyImage}`} src={body.image} alt={`name`} />
-        <span className={`${css.bodyLabel}`}>{name}</span>
-        {headOnly}
+      <div className={css.characterContainer}>
+        <ImageDisplay
+          item={item}
+          className={css.characterImage}
+          showLabel={true}
+        />
       </div>
     )
   }
-}
 
-export default Character
+  // else...
+  const {
+    images: { heads, body },
+  } = images
+
+  const head = heads.find((head) => head.mood === mood) || heads[0]
+
+  const className = showHeadOnly
+    ? css.headOnly
+    : `${css.headForBody} ${isEditMode ? "" : css.noBorder}`
+
+  const headOnly = (
+    <>
+      <Head
+        name={name}
+        head={head}
+        className={headClassName || className}
+        isEditMode={isEditMode}
+      />
+      <span className={`${css.bodyLabel}`}>{name}</span>
+    </>
+  )
+
+  if (showHeadOnly) {
+    return headOnly
+  }
+
+  return (
+    <div className={css.girlBodyContainer}>
+      <img className={`${css.girlBodyImage}`} src={body.image} alt={`name`} />
+      <span className={`${css.bodyLabel}`}>{name}</span>
+      {headOnly}
+    </div>
+  )
+}
