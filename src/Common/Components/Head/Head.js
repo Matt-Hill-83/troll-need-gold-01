4eb0b0e1684d089c9790acdc3035e72d0455b534
Matt Hill <matt.hill.email@gmail.com>
2020-10-26 14:03:49 -0700

    convert Head to function

--- src/Common/Components/Head/Head.js
+++ src/Common/Components/Head/Head.js
@@ -1,36 +1,30 @@
-import React, { Component } from "react"
+import React from "react"
 
 import css from "./Head.module.scss"
 
-class Head extends Component {
-  render() {
-    const {
-      name,
-      head: { image, mood, isEditMode },
-      className = "",
-    } = this.props
+export default function Head(props) {
+  const {
+    name,
+    head: { image, mood, isEditMode },
+    className = "",
+  } = props
 
-    let imageClassName = ""
-    if (name === "kat" || name === "liz2" || name === "chad") {
-      imageClassName = css.girlHeadKat
-    }
-
-    const imageClass = !!imageClassName ? imageClassName : css.girlHeadAmber
-    const containerClass = !!imageClassName ? "" : css.girlHeadAmberContainer
-
-    return (
-      <div
-        className={`${css.girlHeadContainer} ${className}  ${containerClass}`}
-      >
-        <img
-          className={`${css.girlHead} ${imageClass}`}
-          src={image}
-          alt={"mood"}
-        />
-        {isEditMode && <span className={css.moodLabel}>{mood}</span>}
-      </div>
-    )
+  let imageClassName = ""
+  if (name === "kat" || name === "liz2" || name === "chad") {
+    imageClassName = css.girlHeadKat
   }
-}
 
-export default Head
+  const imageClass = !!imageClassName ? imageClassName : css.girlHeadAmber
+  const containerClass = !!imageClassName ? "" : css.girlHeadAmberContainer
+
+  return (
+    <div className={`${css.girlHeadContainer} ${className}  ${containerClass}`}>
+      <img
+        className={`${css.girlHead} ${imageClass}`}
+        src={image}
+        alt={"mood"}
+      />
+      {isEditMode && <span className={css.moodLabel}>{mood}</span>}
+    </div>
+  )
+}
